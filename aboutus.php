ee363a6dd6948085bc4701680ef04e545435b454
MANUJA1 <manujamapalagama@gmail.com>
2016-08-09 21:04:01 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,67 @@
+<?php require 'functions/connection.php'; ?>
+<?php require 'includes/header.php'; ?>
+<?php require 'includes/body.php'; ?>
+
+<link href="css/map.css" type="text/css" rel="stylesheet"> 
+
+<title> view attachment</title>
+
+   
+<body >
+<div class="jumbotron">
+    <center><h1 class="text-red text-display-1 margin-3-3"><font color="blue">About Green University</h1></center>
+    <center><p>Push yourself becouse,no one else is going to do it to you </p></font></center>
+</div>
+
+<div class="about"><img src="img/imageHome0.jpg"></div>
+<div class="col-md-6">
+
+  <colgroup>
+    <col span="20">
+  </colgroup>
+  <tr>
+   
+    <h2 class="text-red text-display-1 margin-3-3"><font color="green"> <ul><li>About Green University</h2></font>
+     <p align="justify">The original Latin word "universitas" refers in general to "a number of
+         persons associated into one body, a society, company,
+         community, guild, corporation, etc." At the time of the emergence of urban town life and medieval guilds,
+         specialized"associations of students and teachers with collective legal rights usually guaranteed by charters
+         issued by princes,prelates, or the towns in which they were located" came to be denominated by this general
+         term. Like other guilds,they were self-regulating and determined the qualifications of their members.<br>
+
+         In modern usage the word has come to mean "An institution of higher education offering tuition in mainly
+         non-vocational subjectsand typically having the power to confer degrees,"with the earlier emphasis on its
+         corporate organization considered asapplying historically to Medieval universities.</p>
+</div>
+
+<div class="col-md-6">
+                <h2 class="text-red text-display-1 margin-3-3"><font color="green"> <li>Mission</h2></font>
+                <p>To advance knowledge and learning through quality research and education for the nation and for humanity.</p>
+
+                <h2 class="text-red text-display-1 margin-3-3"><font color="green"><li>Vision</h2></font>
+                <p>To be an internationally renowned institution of higher learning in research, innovation, publication and teaching.</p>
+
+             <h3 class="text-red text-display-1 margin-3-3"><font color="green"> <li>Goals</h3></font>
+                  
+				  <ul>
+               <p><li>Learning from work placement or employment experiences</li>
+                   <li>Undertaking a volunteering activity</li>
+                   <li>Undertaking a sporting activity</li>
+                   <li>Helping to run a student society</li>
+                   <li>Becoming a member of a Hall of Residence committee or team</li>
+                   <li>Learning about another society or culture</li>
+                   <li>Improving your writing/research skills</li>
+                   <li>Improving your time management skills</li>
+                   <li>Increasing your levels of tutorial contribution</li>
+                   <li>Identifying and applying for study abroad/campus mobility options</li>
+                   <li>Developing ideas for a dissertation topic</li>
+				   </ul>
+				   
+    
+             </p>
+
+  
+</div>    
+</body>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<?php require 'includes/footer.php'; ?>
